e97709f61463a4793704bb8ce3e87a9aec97d8a7
Sallah Kokaina <sallah.kokaina@synaptix-labs.com>
2016-02-09 10:28:49 +0100

    first commit

---
+++ src/main/java/com/synaptix/toast/adapter/swing/component/SwingListElement.java
@@ -0,0 +1,48 @@
+package com.synaptix.toast.adapter.swing.component;
+
+import java.util.UUID;
+
+import com.synaptix.toast.adapter.swing.SwingAutoElement;
+import com.synaptix.toast.adapter.web.HasClickAction;
+import com.synaptix.toast.core.driver.IRemoteSwingAgentDriver;
+import com.synaptix.toast.core.net.request.CommandRequest;
+import com.synaptix.toast.core.runtime.ISwingElementDescriptor;
+import com.synaptix.toast.dao.domain.api.test.ITestResult;
+import com.synaptix.toast.dao.domain.api.test.ITestResult.ResultKind;
+
+public class SwingListElement extends SwingAutoElement implements HasClickAction {
+
+	public SwingListElement(
+		ISwingElementDescriptor element,
+		IRemoteSwingAgentDriver driver) {
+		super(element, driver);
+	}
+
+	public SwingListElement(
+		ISwingElementDescriptor element) {
+		super(element);
+	}
+
+	@Override
+	public ITestResult click()
+		throws Exception {
+		boolean res = exists();
+		final String requestId = UUID.randomUUID().toString();
+		ITestResult result = frontEndDriver.processAndWaitForValue(new CommandRequest.CommandRequestBuilder(requestId).with(wrappedElement.getLocator())
+				.ofType(wrappedElement.getType().name()).click().build());
+		result.setResultKind(res && result.getMessage().equals(ResultKind.SUCCESS.name()) ? ResultKind.SUCCESS : ResultKind.ERROR);
+		return result;
+	}
+
+	@Override
+	public void dbClick() {
+	}
+
+	public void select(
+		String itemName)
+		throws Exception {
+		exists();
+		frontEndDriver.process(new CommandRequest.CommandRequestBuilder(null).with(wrappedElement.getLocator())
+			.ofType(wrappedElement.getType().name()).select(itemName).build());
+	}
+}
